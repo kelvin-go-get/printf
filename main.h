8ef9cf59dbff6fa62b7e4d5cd340333223935536
kelvin <knjuiri@gmail.com>
2023-10-15 14:03:46 -0700

    task 0

--- main.h
+++ main.h
@@ -9,6 +9,11 @@ int _pr_int(va_list arg_list);
 int _printf(const char *format, ...);
 int _putchar(char c);
 void print_buffer(char buffer[], int *buff_ind);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(void);
 
 #endif
 
---
+++ print_str.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of string
+ * @str: string pointer
+ * Return: i
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for(i = 0; str[i] !=0; i++)
+	return (i);
+}
+/**
+ * _strlenc - strlen function
+ * @str: char pointer
+ * Return: i
+ */
+int _strlenc(const char *str)
+{
+	int i;
+
+	for(i = 0; str[i] != 0; i++)
+		return (i);
+}
